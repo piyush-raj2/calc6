d2a075ee2a8e00c6fdff7539bee3c216064b4178
Piyush Raj <piyush.raj@fractalanalytics.com>
2019-02-20 21:24:48 +0530

    Multiply added

--- calc.py
+++ calc.py
@@ -5,11 +5,15 @@ def add(x,y):
 
 def subtract(x,y):
 	return(x-y)
+	
+def multiply(x,y):
+	return(x*y)
 
 def main():
 	a = 7, b = 8
 	print("Add operation for a = ", a, " and b = ", b, " is ", add(a,b))
-	print("Add operation for a = ", a, " and b = ", b," is ", subtract(a,b))
+	print("Subtract operation for a = ", a, " and b = ", b," is ", subtract(a,b))
+	print("Multiply operation for a= ", a, " and b= ", b," is ", multiply(a,b))
 
 
 
