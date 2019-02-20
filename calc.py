1270e587203cd86231d83bf3e699a46f4fde6ead
Piyush Raj <piyush.raj@fractalanalytics.com>
2019-02-20 21:07:41 +0530

    Base calculator code

---
+++ calc.py
@@ -0,0 +1,15 @@
+
+
+def add(x,y):
+	return (x+y)
+
+
+def main():
+	a = 7, b = 8
+	print("Add operation for a = ", a, " and b = ", b, " is ", add(a,b))
+
+
+
+if __name__ == '__main__':
+	main()
+	
